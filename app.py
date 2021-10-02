9f44d201c4089fa34abe0086d3a152a980ac1eb3
Moody Qundeel <moodyandbelia@gmail.com>
2021-10-02 18:38:25 +0200

    set up sqlalchemy database

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask;
+from flask_sqlalchemy import SQLAlchemy;
 
 app = Flask(__name__);
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class User(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+    user = db.relationship("User", backref="user", lazy=True, uselist=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@app.route("/messages")
 def index():
     return "Hello World!"
