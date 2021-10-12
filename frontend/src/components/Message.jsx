b870c6de353c916a9e8f98effc0ec3289668720e
Moody Qundeel <moodyandbelia@gmail.com>
2021-10-13 00:34:19 +0200

    change avatar margin

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,7 +4,7 @@ import "../css/Message.css"
 const Message = (props) => {
     return (
         <div className="message">
-            <Avatar className="message-avatar" sx={{width:35, height:35, mr:2}}>{props.data.user[0] + props.data.user[1]}</Avatar>
+            <Avatar className="message-avatar" sx={{width:35, height:35, mr:1}}>{props.data.user[0] + props.data.user[1]}</Avatar>
             <p>{props.data.message}</p>
         </div>
     );    
